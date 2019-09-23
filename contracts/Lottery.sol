afa01ad6d02d5e40070b42e12de5a3b29b08dcdf
HyoeunKong <hn01360@naver.com>
2019-09-23 23:57:53 +0900

    first commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.8;
+
+contract lottery{
+    address public owner;
+
+    constructor() public{
+        owner = msg.sender;
+    }
+    
+    function getSomeValue() public pure returns (uint256 value){
+        return 5;
+    }
+}
